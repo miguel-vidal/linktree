aa5c385bed23613c9e89b4f81243eb5b7e490a4c
Miguel Angel <angelarkangel91@hotmail.com>
2021-07-01 18:33:04 -0500

    Updates

---
+++ precache-manifest.25449e5b8ba49da6822f65fd9483b3a9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a33a8a189228264a0c7af1720f94e1c7",
+    "url": "/linktree/index.html"
+  },
+  {
+    "revision": "8238d6100fc9b01904a7",
+    "url": "/linktree/static/css/main.3a9ca402.chunk.css"
+  },
+  {
+    "revision": "c29fc2e14769c8cdfab0",
+    "url": "/linktree/static/js/2.7c6d7d42.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/linktree/static/js/2.7c6d7d42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8238d6100fc9b01904a7",
+    "url": "/linktree/static/js/main.ce5c7662.chunk.js"
+  },
+  {
+    "revision": "db98a2433d1abc52cf50",
+    "url": "/linktree/static/js/runtime-main.ae842cb4.js"
+  },
+  {
+    "revision": "879483e47e7b4e21005f2a28c98237f2",
+    "url": "/linktree/static/media/edteam.879483e4.png"
+  },
+  {
+    "revision": "6e83ab4e9ea44d2883c187220fe0e429",
+    "url": "/linktree/static/media/profile.6e83ab4e.jpg"
+  }
+]);
